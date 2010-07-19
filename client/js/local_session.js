1de64a848b3c1af24ebe646d5b4559ad4b5890dc
Schuyler Duveen <sky@skyb.us>
2010-07-18 20:48:31 -0400

    addFRiend, and almost all generateKey

--- client/js/local_session.js
+++ client/js/local_session.js
@@ -1,3 +1,10 @@
+/*
+ {
+    CBOOK_PEOPLE: {"s":['{{key1}}','{{key2}}']}
+    MY_KEYS: {"s":['{{key1}}']}
+    PERSON_{key1}: {key:{friends:{'alias':"{{alias}}"}}}
+ }
+*/
 (function() {
     var global = this;
 
@@ -39,22 +46,56 @@
     }
 
     function MyStorage() {
+	var self = this;
 	this.permStor = new StorageWrapper(hasAttr(global,'localStorage')?global.localStorage:global.globalStorage[location.hostname]);
 	//this.sessStor = new StorageWrapper(global.sessionStorage);
 	this.nsPERSON = 'PERSON_';
 	this.nsPEOPLE = 'CBOOK_PEOPLE';
-	this.nsME = 'ME_';
+	this.nsME = 'MY_KEYS';
+
+	this.getKeyIdentifier = function(key_base64) {
+	    ///first two bytes of the sha256 hash of the key in base64;
+	    return Base64._keyStr[parseInt(
+		ecmaScrypt.sha2.hex_sha256(key_base64).substr(0,2),
+		16)];
+	}
 
 	this.generateKey = function(frm) {
-	    alert(frm.tagName);
+	    var newsecret = Base64.encodeBytes(ecmaScrypt.generateSharedKey(ecmaScrypt.aes.keySize.SIZE_128));
+	    var prefix = self.getKeyIdentifier(newsecret);
+	    var alias = frm.elements['alias'].value;
+	    var obj = {};
+	    ///v:value, p:privacy
+	    obj[newsecret] = {"alias":{"v":alias,"p":"friends"}};
+
+	    self.addFriend(newsecret, obj, prefix);
+	    self.addMyKey(newsecret, obj, prefix);
+	}
+	this.addMyKey = function(newsecret, obj, prefix) {
+	    prefix = prefix || self.getKeyIdentifier(newsecret);
+	    ///TODO NEXT NEXT
+	}
+	this.addFriend = function(newsecret, obj, prefix) {
+	    prefix = prefix || self.getKeyIdentifier(newsecret);
+	    var key = self.nsPERSON + newsecret;
+	    if (key in self.permStor.keyDict()) {
+		var old_person = self.permStor.get(key);
+		if (NOT(obj.alias && obj.alias.v)
+		    || NOT(confirm('You are about to overwrite a preexisting friend/account in your database, Their alias is '+old_person.alias.v+'. The new alias is '+obj.alias.v))) {
+		    return;
+		}
+	    }
+	    if (NOT(obj.alias && obj.alias.v)) {
+		return; //atm, don't support friends w/o aliases
+	    }
+	    self.permStor.set(key, JSON.stringify(obj));
+	    self.addPrefix(prefix, newsecret);
 	}
 
-	this.addFriend = function() {
-	    
-	}
-
-	this.addMyInfo = function() {
-	    
+	this.addPrefix = function(prefix, newsecret) {
+	    var ppl = JSON.parse(self.permStor.get(self.nsPEOPLE));
+	    ppl[prefix] = ppl[prefix] || [];
+	    ppl[prefix].push(newsecret);
 	}
 
 	this.backupFriends = function() {
