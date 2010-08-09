845ec7b1d9ce46b6be160b9bdbc8e9c34c8e8fe2
Schuyler Duveen <sky@skyb.us>
2010-08-09 18:37:21 -0400

    test plan

---
+++ client/js/test.js
@@ -0,0 +1,21 @@
+/*
+-=action ?=test *=store for later part of test
+
+  -*create key1
+  -*encrypt message
+  ?decrypt message
+  -*full backup 
+  -*share
+
+  -delete everything
+  ?restore from full
+     test alias, user
+  ?decrypt message
+
+  -delete everything
+  -*create key2
+  ?restore from share
+  ?decrypt message
+  -encrypt message2 (with key1)
+
+*/
